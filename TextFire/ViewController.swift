e50e8f47fded6b835e18b07e589a6ca225ff1a7e
PiyushxBajaj <piyushpiyush41@gmail.com>
2019-03-21 15:53:24 +0530

    Table view added

--- TextFire/ViewController.swift
+++ TextFire/ViewController.swift
@@ -9,27 +9,43 @@
 import UIKit
 import Firebase
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    
+    //Creating outlets of the table view and text field.
+    @IBOutlet weak var myTableView: UITableView!
     @IBOutlet weak var myText: UITextField!
+    
     var ref : DatabaseReference!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view, typically from a nib.
+            // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    //Creating Action Outlet for the save button.
     @IBAction func saveButton(_ sender: Any) {
         ref = Database.database().reference()
+        
         if myText.text != ""
         {
             ref?.child("list").childByAutoId().setValue(myText.text)
+            //Setting the element in Database
             myText.text = ""
         }
         
     }
+    
+    //Setting up the table view.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
     
 }
 
